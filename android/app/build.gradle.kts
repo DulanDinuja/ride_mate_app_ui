50ad2c7d6687e33bbd67104c8a981c495f90d260
DanushkaNan <danushkanan@lolctech.com>
2026-03-23 20:44:05 +0530

    Changed the application ID.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ride_mate"
+    namespace = "com.ridemate.app"
     compileSdk = 35
     ndkVersion = "27.0.12077973"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ride_mate"
+        applicationId = "com.ridemate.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
