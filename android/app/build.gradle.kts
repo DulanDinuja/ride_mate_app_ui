4fbe32ea8bc01b1be5bb5a32dc0b356129843947
DanushkaNan <danushkanan@lolctech.com>
2026-03-24 16:04:35 +0530

    Updated build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.ridemate.app"
     compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
@@ -25,7 +25,7 @@ android {
         applicationId = "com.ridemate.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
